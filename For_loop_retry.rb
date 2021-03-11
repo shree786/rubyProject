faa90a1f200d6d51e80b885a52412fddc17f10fe
shree786 <80380706+shree786@users.noreply.github.com>
2021-03-11 11:14:48 +0530

    inisital

---
+++ For_loop_retry.rb
@@ -0,0 +1,36 @@
+
+for i in 0..5
+  retry if i>2
+     puts "this is value of #{i}" 
+  end
+
+  for i in 0..5
+    retry if i > 2
+ puts "Value of local variable is #{i}"
+ end
+=begin
+   
+rescue => exception
+   
+end
+ #dont use redo keyword this is like deadlock
+ for i in 0..5
+   if i < 2 then
+      puts "Value of local variable is #{i}"
+      redo
+   end
+end
+=end
+=begin
+   
+rescue => exception
+   
+end
+for i in 0..5
+   if i <= 2 then
+     next
+   else
+      puts"thuis is greater then 2 #{i}"
+   end
+end
+=end
